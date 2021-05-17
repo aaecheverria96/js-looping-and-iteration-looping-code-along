05fd448eeafaf9174808ac5a9312a5113d9cd745
Your Username <you@example.com>
2021-05-17 14:13:25 -0400

    commit tree f7c96ca2c1eaf3689a89ad48270693e87ad7fa3f

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(array, eventName) { 
+   let thankYouCards = []
+    for (let i = 0; i < array.length; i++) { 
+       thankYouCards.push(`Thank you, ${array[i]}, for the wonderful ${eventName} gift!`)
+    } 
+    return thankYouCards
+} 
+
+function countDown(num) { 
+    while (num >= 0) { 
+        console.log(num) 
+        num-- 
+    }
+}
